8e08049dcf92c1ec24d3100b5f38d3e05c648c2a
rubens21 <rubens21@gmail.com>
2022-01-15 11:49:53 -0500

    adjusting position to make the team more aggressive

--- bot/tactic.go
+++ bot/tactic.go
@@ -24,35 +24,34 @@ func DefineRole(number uint32) Role {
 	return ""
 }
 
-func DetermineTeamState(ballRegion field.Region, teamSide, possession lugo.Team_Side) (s TeamState, e error) {
+func DetermineTeamState(ballRegion field.Region, myTeamSide, possession lugo.Team_Side) (s TeamState, e error) {
 	regionCol := ballRegion.Col()
-	if possession == teamSide {
+	if possession == myTeamSide {
 		switch regionCol {
-		case 6, 7:
+		case 7, 8, 9:
 			return OnAttack, nil
-		case 4, 5:
+		case 6:
 			return Offensive, nil
-		case 3:
+		case 3, 4, 5:
 			return Neutral, nil
-		case 2:
+		case 1, 2:
 			return Defensive, nil
-		case 0, 1:
+		case 0:
 			return UnderPressure, nil
 		}
 
 	} else {
 		switch regionCol {
-		case 6, 7:
-			return OnAttack, nil
-		case 4, 5:
-			return Offensive, nil
-		case 3:
-			return Neutral, nil
-		case 2:
+		case 9:
 			return Defensive, nil
-		case 0, 1:
+		case 6, 7, 8:
+			return Defensive, nil
+		case 3, 4, 5:
+			return Defensive, nil
+		case 0, 1, 2:
 			return UnderPressure, nil
 		}
+		//return Offensive, nil
 	}
 	return "", fmt.Errorf("unknown team state for ball in %d col, tor possion with %s", regionCol, possession)
 }
@@ -61,82 +60,82 @@ var roleMap = map[uint32]PlayerActionRegions{
 	// starting from 2 because the number goalkeeper has PlayerActionRegions
 	2: {
 		Initial:       {1, 2},
-		UnderPressure: {0, 1},
-		Defensive:     {1, 0},
-		Neutral:       {2, 0},
-		Offensive:     {1, 0},
-		OnAttack:      {1, 1},
+		UnderPressure: {0, 3},
+		Defensive:     {1, 3},
+		Neutral:       {2, 3},
+		Offensive:     {2, 3},
+		OnAttack:      {4, 3},
 	},
 	3: {
 		Initial:       {1, 5},
-		UnderPressure: {1, 1},
-		Defensive:     {1, 1},
-		Neutral:       {1, 1},
-		Offensive:     {3, 1},
-		OnAttack:      {4, 1},
+		UnderPressure: {0, 4},
+		Defensive:     {1, 4},
+		Neutral:       {2, 4},
+		Offensive:     {2, 4},
+		OnAttack:      {4, 4},
 	},
 	4: {
 		Initial:       {2, 6},
-		UnderPressure: {1, 2},
-		Defensive:     {1, 2},
-		Neutral:       {1, 2},
-		Offensive:     {3, 2},
-		OnAttack:      {4, 2},
+		UnderPressure: {1, 6},
+		Defensive:     {2, 6},
+		Neutral:       {3, 6},
+		Offensive:     {4, 5},
+		OnAttack:      {5, 5},
 	},
 	5: {
 		Initial:       {2, 1},
-		UnderPressure: {0, 2},
-		Defensive:     {1, 3},
-		Neutral:       {2, 3},
-		Offensive:     {1, 3},
-		OnAttack:      {1, 2},
+		UnderPressure: {1, 1},
+		Defensive:     {1, 1},
+		Neutral:       {3, 1},
+		Offensive:     {5, 2},
+		OnAttack:      {5, 2},
 	},
 	6: {
 		Initial:       {3, 6},
-		UnderPressure: {1, 0},
-		Defensive:     {2, 1},
-		Neutral:       {3, 1},
-		Offensive:     {4, 1},
-		OnAttack:      {5, 0},
+		UnderPressure: {1, 5},
+		Defensive:     {2, 5},
+		Neutral:       {4, 5},
+		Offensive:     {5, 6},
+		OnAttack:      {6, 6},
 	},
 	7: {
-		Initial:       {3, 4},
-		UnderPressure: {2, 1},
-		Defensive:     {3, 1},
-		Neutral:       {4, 1},
+		Initial:       {3, 1},
+		UnderPressure: {1, 2},
+		Defensive:     {2, 2},
+		Neutral:       {4, 2},
 		Offensive:     {5, 1},
 		OnAttack:      {6, 1},
 	},
 	8: {
 		Initial:       {3, 3},
-		UnderPressure: {2, 2},
-		Defensive:     {3, 2},
-		Neutral:       {4, 2},
-		Offensive:     {5, 2},
-		OnAttack:      {6, 2},
+		UnderPressure: {1, 3},
+		Defensive:     {2, 3},
+		Neutral:       {4, 3},
+		Offensive:     {6, 2},
+		OnAttack:      {7, 2},
 	},
 	9: {
-		Initial:       {3, 1},
-		UnderPressure: {1, 3},
-		Defensive:     {2, 2},
-		Neutral:       {3, 2},
-		Offensive:     {4, 2},
-		OnAttack:      {5, 3},
+		Initial:       {3, 4},
+		UnderPressure: {1, 4},
+		Defensive:     {3, 4},
+		Neutral:       {4, 4},
+		Offensive:     {6, 5},
+		OnAttack:      {7, 4},
 	},
 	10: {
 		Initial:       {4, 5},
-		UnderPressure: {3, 1},
-		Defensive:     {4, 1},
-		Neutral:       {5, 1},
-		Offensive:     {6, 1},
-		OnAttack:      {7, 1},
+		UnderPressure: {2, 5},
+		Defensive:     {4, 5},
+		Neutral:       {5, 6},
+		Offensive:     {7, 5},
+		OnAttack:      {8, 5},
 	},
 	11: {
 		Initial:       {4, 2},
-		UnderPressure: {3, 2},
+		UnderPressure: {2, 2},
 		Defensive:     {4, 2},
-		Neutral:       {6, 2},
-		Offensive:     {6, 2},
-		OnAttack:      {7, 2},
+		Neutral:       {5, 1},
+		Offensive:     {7, 2},
+		OnAttack:      {9, 2},
 	},
 }
