db9b19c93ed283e96c2a23f8ab5a74654e24fb64
rubens21 <rubens21@gmail.com>
2022-09-12 17:04:22 -0600

    fixing initialization

--- wait_to_compile.sh
+++ wait_to_compile.sh
@@ -2,10 +2,22 @@
 
 max_retry=10
 counter=0
-until /app/my_bot_binary
+
+FILE=/app/my_bot_binary
+
+# let's wait just a bit to ensure the old binary was already deleted
+sleep 0.3
+
+if [ ! -f "$FILE" ]; then
+  sleep 0.5
+fi
+
+until [[ -f "$FILE" ]]
 do
-   sleep 3
+   sleep 0.5
    [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
    echo "Trying again. Try #$counter"
    ((counter++))
 done
+
+/app/my_bot_binary
