3bbd0fdd9a596d15dbbaffe3dd517ad67d0f4c49
rubens21 <rubens21@gmail.com>
2022-09-12 18:14:40 -0600

    simplifying the logic

--- bot/bot.go
+++ bot/bot.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"context"
+
 	"github.com/lugobots/lugo4go/v2"
 	"github.com/lugobots/lugo4go/v2/pkg/field"
 	"github.com/lugobots/lugo4go/v2/proto"
@@ -19,8 +20,6 @@ type Bot struct {
 	lastKickTurn uint32
 }
 
-const afterKickingWaitingTime = uint32(8)
-
 func NewBot(logger lugo4go.Logger, side proto.Team_Side, number uint32) *Bot {
 	fieldMapper, _ := field.NewMapper(FieldGridCols, FieldGridRows, side)
 
@@ -36,6 +35,151 @@ func NewBot(logger lugo4go.Logger, side proto.Team_Side, number uint32) *Bot {
 	}
 	return &me
 }
+
+func (b *Bot) OnDisputing(ctx context.Context, sender lugo4go.TurnOrdersSender, snapshot *proto.GameSnapshot) error {
+	me := field.GetPlayer(snapshot, b.side, b.number)
+
+	ballPosition := snapshot.GetBall().GetPosition()
+
+	ballRegion, err := b.mapper.GetPointRegion(ballPosition)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the ball region")
+	}
+
+	myRegion, err := b.mapper.GetPointRegion(me.GetPosition())
+	if err != nil {
+		return errors.Wrap(err, "failed to find my region")
+	}
+
+	// if the ball is max 2 blocks away from me, I will move toward the ball
+	if !isNear(myRegion, ballRegion) {
+		return b.holdPosition(ctx, sender, snapshot)
+	}
+
+	moveOrder, err := field.MakeOrderMoveMaxSpeed(*me.Position, *ballPosition)
+	if err != nil {
+		return errors.Wrap(err, "error creating move order")
+	}
+
+	// we can ALWAYS try to catch the ball if we are not holding it
+	catchOrder := field.MakeOrderCatch()
+
+	return processServerResp(sender.Send(ctx, []proto.PlayerOrder{moveOrder, catchOrder}, "trying to catch the ball"))
+
+}
+
+func (b *Bot) OnDefending(ctx context.Context, sender lugo4go.TurnOrdersSender, snapshot *proto.GameSnapshot) error {
+	me := field.GetPlayer(snapshot, b.side, b.number)
+
+	ballPosition := snapshot.GetBall().GetPosition()
+
+	ballRegion, err := b.mapper.GetPointRegion(ballPosition)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the ball region")
+	}
+
+	myRegion, err := b.mapper.GetPointRegion(me.GetPosition())
+	if err != nil {
+		return errors.Wrap(err, "failed to find my region")
+	}
+
+	// if the ball is max 2 blocks away from me, I will move toward the ball
+	if !isNear(myRegion, ballRegion) {
+		return b.holdPosition(ctx, sender, snapshot)
+	}
+
+	moveOrder, err := field.MakeOrderMoveMaxSpeed(*me.Position, *ballPosition)
+	if err != nil {
+		return errors.Wrap(err, "error creating move order")
+	}
+
+	// we can ALWAYS try to catch the ball if we are not holding it
+	catchOrder := field.MakeOrderCatch()
+
+	return processServerResp(sender.Send(ctx, []proto.PlayerOrder{moveOrder, catchOrder}, "trying to catch the ball"))
+}
+
+func (b *Bot) OnHolding(ctx context.Context, sender lugo4go.TurnOrdersSender, snapshot *proto.GameSnapshot) error {
+	me := field.GetPlayer(snapshot, b.side, b.number)
+
+	goal := field.GetOpponentGoal(b.side)
+
+	goalRegion, err := b.mapper.GetPointRegion(&goal.Center)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the ball region")
+	}
+
+	myRegion, err := b.mapper.GetPointRegion(me.GetPosition())
+	if err != nil {
+		return errors.Wrap(err, "failed to find my region")
+	}
+
+	var order proto.PlayerOrder
+	// if we are near to the goal, let's kick it!
+	if !isNear(myRegion, goalRegion) {
+		return b.holdPosition(ctx, sender, snapshot)
+
+	}
+
+	order, err = field.MakeOrderKickMaxSpeed(*snapshot.GetBall(), goal.Center)
+	if err != nil {
+		return errors.Wrap(err, "failed to create kick order")
+	}
+
+	return processServerResp(sender.Send(ctx, []proto.PlayerOrder{order}, "attack!"))
+}
+
+func (b *Bot) OnSupporting(ctx context.Context, sender lugo4go.TurnOrdersSender, snapshot *proto.GameSnapshot) error {
+	me := field.GetPlayer(snapshot, b.side, b.number)
+
+	teammatePosition := snapshot.GetBall().GetHolder().GetPosition()
+
+	teammateRegion, err := b.mapper.GetPointRegion(teammatePosition)
+	if err != nil {
+		return errors.Wrap(err, "failed to find the teammate region")
+	}
+
+	myRegion, err := b.mapper.GetPointRegion(me.GetPosition())
+	if err != nil {
+		return errors.Wrap(err, "failed to find my region")
+	}
+
+	// if the player mate is max 2 blocks away from me, I will help
+	if !isNear(myRegion, teammateRegion) {
+		return b.holdPosition(ctx, sender, snapshot)
+	}
+
+	moveOrder, err := field.MakeOrderMoveMaxSpeed(*me.Position, *teammatePosition)
+	if err != nil {
+		return errors.Wrap(err, "error creating move order")
+	}
+
+	return processServerResp(sender.Send(ctx, []proto.PlayerOrder{moveOrder}, "trying to catch the ball"))
+}
+
+func (b *Bot) AsGoalkeeper(ctx context.Context, sender lugo4go.TurnOrdersSender, snapshot *proto.GameSnapshot, myState lugo4go.PlayerState) error {
+	me := field.GetPlayer(snapshot, b.side, b.number)
+
+	if myState == lugo4go.HoldingTheBall {
+		order, err := field.MakeOrderKickMaxSpeed(*snapshot.GetBall(), field.FieldCenter())
+		if err != nil {
+			return errors.Wrap(err, "failed to create kick order")
+		}
+
+		return processServerResp(sender.Send(ctx, []proto.PlayerOrder{order}, "kick the ball"))
+	}
+
+	ballPosition := snapshot.GetBall().GetPosition()
+
+	moveOrder, err := field.MakeOrderMoveMaxSpeed(*me.Position, *ballPosition)
+	if err != nil {
+		return errors.Wrap(err, "error creating move order")
+	}
+
+	return processServerResp(sender.Send(ctx, []proto.PlayerOrder{moveOrder, field.MakeOrderCatch()}, "trying to catch the ball"))
+}
+
+// Aux methods
 
 func (b *Bot) MyInitialPosition() *proto.Point {
 	iniRegion := b.actionRegions[Initial]
--- bot/settings.go
+++ bot/settings.go
@@ -1,7 +1,6 @@
 package bot
 
 import (
-	"github.com/lugobots/lugo4go/v2/pkg/field"
 	"math/rand"
 	"time"
 )
@@ -35,23 +34,6 @@ const (
 	Attack  Role = "attack"
 )
 
-type FuzzyScale int
-
-const (
-	MustNot FuzzyScale = iota
-	ShouldNot
-	May
-	Should
-	Must
-)
-
-const (
-	DistanceBeside = field.FieldWidth / 10
-	DistanceNear   = field.FieldWidth / 8
-	DistanceFar    = field.FieldWidth / 6
-	DistanceTooFar = field.FieldWidth / 4
-)
-
 type RegionCode struct {
 	Col uint8
 	Row uint8
@@ -61,4 +43,93 @@ type PlayerActionRegions map[TeamState]RegionCode
 
 func DefinePlayerActionRegions(number uint32) PlayerActionRegions {
 	return roleMap[number]
+}
+
+const (
+	FieldGridCols = 10
+	FieldGridRows = 8
+)
+
+var roleMap = map[uint32]PlayerActionRegions{
+	// starting from 2 because the number goalkeeper has PlayerActionRegions
+	2: {
+		Initial:       {1, 2},
+		UnderPressure: {0, 3},
+		Defensive:     {1, 3},
+		Neutral:       {2, 3},
+		Offensive:     {2, 3},
+		OnAttack:      {5, 3},
+	},
+	3: {
+		Initial:       {1, 5},
+		UnderPressure: {0, 4},
+		Defensive:     {1, 4},
+		Neutral:       {2, 4},
+		Offensive:     {2, 4},
+		OnAttack:      {5, 4},
+	},
+	4: {
+		Initial:       {2, 6},
+		UnderPressure: {1, 6},
+		Defensive:     {2, 6},
+		Neutral:       {3, 6},
+		Offensive:     {5, 5},
+		OnAttack:      {6, 5},
+	},
+	5: {
+		Initial:       {2, 1},
+		UnderPressure: {1, 1},
+		Defensive:     {2, 1},
+		Neutral:       {3, 1},
+		Offensive:     {5, 2},
+		OnAttack:      {6, 2},
+	},
+	6: {
+		Initial:       {3, 6},
+		UnderPressure: {1, 5},
+		Defensive:     {2, 5},
+		Neutral:       {4, 5},
+		Offensive:     {8, 7},
+		OnAttack:      {9, 6},
+	},
+	7: {
+		Initial:       {3, 1},
+		UnderPressure: {1, 2},
+		Defensive:     {2, 2},
+		Neutral:       {4, 2},
+		Offensive:     {8, 0},
+		OnAttack:      {9, 1},
+	},
+	8: {
+		Initial:       {3, 3},
+		UnderPressure: {1, 3},
+		Defensive:     {3, 3},
+		Neutral:       {4, 3},
+		Offensive:     {6, 3},
+		OnAttack:      {7, 3},
+	},
+	9: {
+		Initial:       {3, 4},
+		UnderPressure: {1, 4},
+		Defensive:     {3, 4},
+		Neutral:       {4, 4},
+		Offensive:     {6, 4},
+		OnAttack:      {7, 4},
+	},
+	10: {
+		Initial:       {4, 5},
+		UnderPressure: {2, 5},
+		Defensive:     {4, 5},
+		Neutral:       {5, 6},
+		Offensive:     {7, 5},
+		OnAttack:      {8, 5},
+	},
+	11: {
+		Initial:       {4, 2},
+		UnderPressure: {2, 2},
+		Defensive:     {4, 2},
+		Neutral:       {5, 1},
+		Offensive:     {7, 2},
+		OnAttack:      {9, 2},
+	},
 }
